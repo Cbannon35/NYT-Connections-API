36c352edf374f0b910a09affff0fba5abe5747b9
Christopher Bannon <bannon.c.35@berkeley.edu>
2024-04-28 22:14:08 -0700

    remove port?

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 
 def main():
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
+    uvicorn.run("server.app:app", host="0.0.0.0")
 
 if __name__ == "__main__":
     main()
