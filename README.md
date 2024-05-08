90500b1b122d281685ab59a26c41e511465ff87c
Christopher Bannon <bannon.c.35@berkeley.edu>
2024-05-07 19:42:17 -0700

    branch for webscraper

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NYT-Connections-API
+
+This branch is where work on the webscraper for the NYT Connections API will be done.
--- app/main.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-
-def main():
-    uvicorn.run("server.app:app", host="0.0.0.0")
-
-if __name__ == "__main__":
-    main()
